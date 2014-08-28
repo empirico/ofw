f6e1f50d6e8eca069d7c6a6ee8381deb178c419e
unknown <daniele.murroni@LEBL-MURRONI.triboo.local>
2014-08-28 11:44:07 +0200

    load home events

--- www/js/ofw-app.js
+++ www/js/ofw-app.js
@@ -1,30 +1,71 @@
 var map;
 var ofw = {
 
-	init: function(){
+	init: function(e){
 		
-		$(window).load(function() {
-			var  center_start = new google.maps.LatLng(45.4654219,9.1859243);
-			
-			/*
-				map = ofw.utils.init_gmap("home_map", {
-					center: center_start
-				});	
-			*/
+		//e.type = "ready" -> no phonegap
+		//e.type = "deviceready" -> phonegap
+		//console.log(e);
+		//INIT FW7APP
+		window.ofwF7App = new Framework7();
+		window.$$ = Dom7;
+		window.mainView = ofwF7App.addView('.view-main', {
+		            // Because we use fixed-through navbar we can enable dynamic navbar
+		            dynamicNavbar: true
 		});
+
+		ofw.build_controls()
+		ofw.events();
 		
-		$$("#create-event").on('click', function() {
-
-			$.get("event.html", function(data){
-
-				$html = '<div class="popup">' + data + "</div>";
-				ofwF7App.popup($html);
-
-			});
-		});
+		
 
 	},
+	build_controls: function() {
+		//bind homepage events
+		$$("#create-event").on('click', function() {
+					$.get("event.html", function(data){
+
+						$html = '<div class="popup">' + data + "</div>";
+						ofwF7App.popup($html);
+
+					});
+		});
+	},
 	events: function(){
+		
+		var template;
+		var load_events = function($url, $target, acallback) {
+			$.ajax({
+				  dataType: "json",
+				  url: $url,
+				  success: function(data) {
+							$(data).each(function(i, el) {
+								$item = Mustache.render(template, el)
+								$target.append($item);
+							})
+							acallback(null, "events-loaded");
+						}
+				});
+		}
+		$.get("tpl/event_item.mst", function(tpl) { 
+			template = tpl;
+			var parallel_tasks = [function(callback){
+									load_events("json/events_nearby.json", $("#home-tab-nearby-ofws ul"), callback);
+
+									},
+									function(callback){
+										load_events("json/events_friends.json", $("#home-tab-friends-ofws ul"), callback)
+								}];
+			async.parallel(parallel_tasks, function(){
+					console.log("async finished");
+					$("#home-events-tabs").removeClass("loading");
+			});
+			
+		});
+
+		
+			
+		
 
 	},
 	utils: {
@@ -44,5 +85,5 @@ var ofw = {
 
 }
 
-
-ofw.init();
+var ready_event =  (window.cordova) ? "deviceready" : "ready";
+$(document).on(ready_event, ofw.init);
